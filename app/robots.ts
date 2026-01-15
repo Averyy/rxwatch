dc7949fd364f0334d7ef7b765d8f46cf0b1b569a
Avery Levitt <avery23@gmail.com>
2026-01-15 14:04:44 -0500

    Serve sitemap index at /sitemap.xml via rewrite

--- app/robots.ts
+++ app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: '/',
       },
     ],
-    sitemap: `${baseUrl}/api/sitemap-index`,
+    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
